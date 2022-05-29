5593e78f4232baaa7e1eb19254ac43c8c1f479f8
Gina Henderson <gina@ginahenderson.me>
2022-05-29 06:26:52 -0500

    Create new list function

--- src/DOM-elements/to-do.js
+++ src/DOM-elements/to-do.js
@@ -1,7 +1,15 @@
 import { v4 as uuidv4 } from "uuid";
+import save from "../functions/save";
+import load from "../functions/load";
+
+const profile = load('profile');
 
 export default project => {
-    
+
+    console.log(project)
+
+    const list = createList(project);
+
     const todoListContainer = document.createElement('div');
     todoListContainer.classList.add('to-do-list-container');
 
@@ -9,19 +17,36 @@ export default project => {
     todoListContainer.append(todoList);
     todoList.classList.add('to-do-list');
     todoList.classList.add('drag-container');
-    todoList.id = `${project.id}.${uuidv4()}`;
+    todoList.id = list.id;
 
     const button = document.createElement('button');
     todoListContainer.append(button);
     button.classList.add('add-new-task');
+    button.addEventListener('click', e => {
+        createTask(list);
+    });
+    button.textContent = 'Add new task';
 
     return todoListContainer;
 }
 
 const createList = project => {
+    const list = {
+        id: `${project.id}.${uuidv4()}`,
+        tasks: [],
+        container: project.id,
+    }
+    project.lists.push(list);
+    save('profile', profile);
+    console.log(project);
+    return list;
+}
 
+const createTaskNode = list => {
+    
 }
 
 const createTask = list => {
+    console.log(`Will create task with id ${list.id}.${uuidv4()}`);
 
 }
